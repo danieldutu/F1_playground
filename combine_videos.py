849dc0b6890d71c818f9eb1b27a2e9688483614a
Daniel Dutu <daniel.dutu@porsche-engineering.ro>
2023-02-04 18:05:39 +0200

    added requirements

--- combine_videos.py
+++ combine_videos.py
@@ -7,23 +7,5 @@ command = [r'D:\ffmpeg-2022-05-12-git-30e2bb0f64-essentials_build\bin\ffmpeg.exe
            '-vsync', 'vfr', 'output.mp4']
 
 
-
-# def ffmpeg_command_concat(ffmpeg_path, bev_video_path, out_file_path, out_file_path_cut, fps):
-#     bev_video = f'{ffmpeg_path} -y -vsync 2 -r {fps:0.01f} -i "{bev_video_path}"'
-#     camera_video = f'-i "{out_file_path_cut}"' ' -filter_complex "[1][0]scale2ref=oh*mdar:ih[camera][bev];[camera][bev]hstack"'
-#     output_file = f'"{out_file_path}"'
-#     commands_arr = [bev_video, camera_video, output_file]
-
-#     return ' '.join(commands_arr)
-
-# bev_command = ffmpeg_command_concat(ffmpeg_path = r'D:\ffmpeg-2022-05-12-git-30e2bb0f64-essentials_build\bin\ffmpeg.exe', 
-#                                     bev_video_path = r'D:\F1_playground\telemetry.mp4', #r'D:\F1_playground\ver_lap_edited.mp4', 
-#                                     out_file_path = r'D:\F1_playground\output.mp4', 
-#                                     out_file_path_cut= r'D:\F1_playground\ver_lap_edited.mp4',  
-#                                     fps = 50)
-# subprocess.run(bev_command)
-
-
-
 subout = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 print(subout.stdout)
